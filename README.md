9d60d1e32f4adc190e6aea83e255bcefffb43d23
Alex Nguyen <alex991nguyen@gmail.com>
2021-02-11 00:44:34 -0800

    (chore) update README & adding debugMode for apiGateway

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # Display Cache Webpage
 
-I created this webpage to learn [Vue.js](https://vuejs.org/).
+I created this project to learn [Vue.js](https://vuejs.org/).
 
-It simply displays the a bunch of tweets. The [Amazon API Gateway](https://aws.amazon.com/api-gateway/)
+It simply displays a bunch of tweets. The [Amazon API Gateway](https://aws.amazon.com/api-gateway/)
 it uses is created from **[CacheTwitterCdk](https://github.com/alexhnguyen/CacheTwitterCdk)**.
 
 ![sample](images/2021-02-11.png)
+
+## Install
+
+Initial package creation
+
+```
+yarn global add @vue/cli
+yarn global add @vue/cli @vue/cli-service-global
+vue create <project name>
+yarn add axios
+yarn install
+```
+
+To start the project
+
+```
+yarn serve
+```
+
+## TODO
+
+- ~~query API Gateway using [axios](https://www.npmjs.com/package/axios), and then display it~~
+- create a component for each Tweet
--- src/App.vue
+++ src/App.vue
@@ -50,12 +50,21 @@ export default {
   },
   methods: {
     async fetchItems() {
-      // put apiGateway into here
-      await axios.get("").then((response) => {
-        for (const obj of eval(response.data)) {
+      // debugMode if you do not want to start up an API Gateway
+      const debugMode = true;
+      if (debugMode) {
+        const responseData =
+          "[{'author_id': 813286, 'screen_name': 'BarackObama', 'created_at': '2021-01-31T16:00:34', 'text': \"When Jackie Robinson faced down slurs, spiked cleats, and pitches aimed at his head—and stole home anyway—he didn't… https://t.co/CwkfJf2aNb\", 'favorite_count': 258399, 'retweet_count': 26343}, {'author_id': 813286, 'screen_name': 'BarackObama', 'created_at': '2021-02-02T13:41:59', 'text': 'Black-owned independent bookstores play a critical role in communities all across our country. I wanted to show my… https://t.co/o1a6dVhhuz', 'favorite_count': 47662, 'retweet_count': 5782}, {'author_id': 813286, 'screen_name': 'BarackObama', 'created_at': '2021-02-03T20:31:05', 'text': 'I’m proud to announce that the Obama Presidential Center will officially break ground in 2021. Our hope is that the… https://t.co/WDfSpstiQr', 'favorite_count': 146637, 'retweet_count': 14631}, {'author_id': 813286, 'screen_name': 'BarackObama', 'created_at': '2021-02-05T17:30:44', 'text': 'Climate\xa0change\xa0is a real threat to us all—but feeling helpless or overwhelmed isn’t going to solve it. As long as w… https://t.co/ospbfmqv4e', 'favorite_count': 47458, 'retweet_count': 5525}, {'author_id': 813286, 'screen_name': 'BarackObama', 'created_at': '2021-02-09T16:30:46', 'text': \"There is a lot of disinformation out there, but here’s the truth: You should get a COVID vaccine as soon as it's av… https://t.co/7bxIfHbHks\", 'favorite_count': 55303, 'retweet_count': 7902}]";
+        for (const obj of eval(responseData)) {
           this.tableData.push(obj);
         }
-      });
+      } else {
+        await axios.get("apiGatewayAddress").then((response) => {
+          for (const obj of eval(response.data)) {
+            this.tableData.push(obj);
+          }
+        });
+      }
     },
   },
   name: "App",
