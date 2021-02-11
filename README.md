c3067cbb5892fc57ab60b74cc46ad6b71f64b062
Alex Nguyen <alex991nguyen@gmail.com>
2021-02-10 23:58:33 -0800

    (feat) display twitter data

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
-# hello-world
+# Display Cache Webpage
 
-## Project setup
-```
-yarn install
-```
+I created this webpage to learn [Vue.js](https://vuejs.org/).
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+It simply displays the a bunch of tweets. The [Amazon API Gateway](https://aws.amazon.com/api-gateway/)
+it uses is created from **[CacheTwitterCdk](https://github.com/alexhnguyen/CacheTwitterCdk)**.
 
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+![sample](images/2021-02-11.png)
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div id="dataStuff">
+      <tr style="font-weight: bold">
+        <td>
+          author_id
+        </td>
+        <td>
+          screen_name
+        </td>
+        <td>
+          created_at
+        </td>
+        <td>
+          text
+        </td>
+      </tr>
+      <tr v-for="row in tableData" :key="row.id">
+        <td>
+          {{ row.author_id }}
+        </td>
+        <td>
+          {{ row.screen_name }}
+        </td>
+        <td>
+          {{ row.created_at }}
+        </td>
+        <td>
+          {{ row.text }}
+        </td>
+      </tr>
+      <br />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 export default {
+  async created() {
+    await this.fetchItems();
+  },
+  data() {
+    return {
+      tableData: [],
+    };
+  },
+  methods: {
+    async fetchItems() {
+      // put apiGateway into here
+      await axios.get("").then((response) => {
+        for (const obj of eval(response.data)) {
+          this.tableData.push(obj);
+        }
+      });
+    },
+  },
   name: "App",
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+  },
 };
 </script>
 
@@ -24,5 +73,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+td {
+  padding: 10px;
 }
 </style>
